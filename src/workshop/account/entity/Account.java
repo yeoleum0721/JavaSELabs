ba2d935c2bc103c481861d4118619d54c6e904cc
yeoleum0721 <jooyun0229@gmail.com>
2025-08-06 14:52:49 +0900

    java 기본

---
+++ src/workshop/account/entity/Account.java
@@ -0,0 +1,53 @@
+package workshop.account.entity;
+
+public class Account {
+	private String custId;
+	private String acctId;
+	private int balance;
+	
+	//default constructor 선언
+	public Account() {
+		System.out.println("기본생성자 호출됨");
+	}
+	
+	//Constructor Overloading(생성자 중복정의)
+	public Account(String custId, String acctId, int balance) {
+		//this.custId = custId;
+		//this.acctId = acctId;
+		setCustId(custId);
+		setAcctId(acctId);
+		this.balance = balance;
+	}
+
+
+//	public void setBalance(int balance) {
+//		this.balance = balance;
+//	}
+	public int getBalance() {
+		return balance;
+	}
+	
+	public void setAcctId(String acctId) {
+		this.acctId = acctId;
+	}
+	public String getAcctId() {
+		return acctId;
+	}
+
+	public String getCustId() {
+		return custId;
+	}
+
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+	//deposit
+	public void deposit(int amount) {
+		this.balance += amount;
+	}
+	//withdraw
+	public void withdraw(int amount) {
+		this.balance -= amount;
+	}
+
+}
